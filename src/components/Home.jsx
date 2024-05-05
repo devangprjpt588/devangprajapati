b5eb04636b5814995c8afda8054cfa2046ae73b9
Devang <devangprjpt@gmail.com>
2024-05-06 01:33:56 +0530

    Cursor pointer added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
               <h2 className='text-2xl xl:text-5xl text-slate-600 pt-4'>UI Developer</h2>
               <p className="py-8 md:w-2/3 text-slate-600">Passionate about crafting engaging user experiences through clean code and intuitive designs</p>
               <div className='flex'>
-                <Link to="skills" smooth={true} duration={500} className='group rounded-lg border-2 border-sky-600 text-sky-600 hover:text-white hover:bg-sky-600 px-4 py-2 flex justify-center items-center'>My Skills <span className='group-hover:rotate-90 duration-300'><HiArrowNarrowRight className='ml-3'/></span></Link>
+                <Link to="skills" smooth={true} duration={500} className='group cursor-pointer rounded-lg border-2 border-sky-600 text-sky-600 hover:text-white hover:bg-sky-600 px-4 py-2 flex justify-center items-center'>My Skills <span className='group-hover:rotate-90 duration-300'><HiArrowNarrowRight className='ml-3'/></span></Link>
                 <Link to="about" smooth={true} duration={500} className='group cursor-pointer rounded-lg border-2 border-sky-600 text-white hover:text-sky-600 bg-sky-600 hover:bg-white px-4 py-2 flex justify-center items-center ml-4'>Scroll Down <span className='animate-bounce'><FaAngleDoubleDown className='ml-3'/></span></Link>
               </div>
             </div>
